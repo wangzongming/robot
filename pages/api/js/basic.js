14448ff498ef6999e6414879e60a90be082ee58f
xm <1746809408@qq.com>
2020-10-31 12:37:55 +0800

    更新

--- pages/api/js/basic.js
+++ pages/api/js/basic.js
@@ -7,8 +7,8 @@ export default (req, res) => {
 
     let resJson = {
         author: "jonas",
-        code: `暂未获取到， 如果您有好的代码段欢迎提交github：https://github.com/wangzongming/robot`,
-        success:false
+        code: `暂未获取到您想要的回复， 如果您有好的代码段欢迎提交github：https://github.com/wangzongming/robot`,
+        success: false
     };
 
     forLabel:
@@ -19,11 +19,12 @@ export default (req, res) => {
             author
         } = funcs[i];
         //模糊匹配
-        const isOk = keywords.filter((item) => new RegExp(inpKeyword, "ig").test(item))[0];
-        if (isOk) { 
-            resJson = { author, code, success:true } 
+        const isOk = keywords.filter((item) => new RegExp(inpKeyword, "ig").test(item))[0]; //正向匹配
+        const isOk2 = keywords.filter((item) => new RegExp(item, "ig").test(inpKeyword))[0]; //逆向匹配
+        if (isOk || isOk2) {
+            resJson = { author, code, success: true }
             break forLabel;
-        } 
+        }
     }
 
     res.json(resJson);
--- pages/api/js/func.js
+++ pages/api/js/func.js
@@ -3,7 +3,7 @@
 const func = [
     {
         "author":"jonas",
-        "keywords":["周日期", "获取当前所在周的起始和结束的日期", "获取当前所在周的起始日期", "获取当前所在周的结束日期"],
+        "keywords":["周日期", "当前周的起始时间", "获取当前所在周的起始和结束的日期", "获取当前所在周的起始日期", "获取当前所在周的结束日期"],
         "code":`
             /**
              * 获取当前星期的起始日期和结束日期
@@ -76,6 +76,81 @@ const func = [
                     }
                 },
             };
+        `
+    },
+    {
+        "author":"jonas",
+        "keywords":["js字符串翻转", "字符串翻转", "字符串倒过来"],
+        "code":`
+            var str = 'abcdefg';
+            var revs = str.split('').reverse().join(''); 
+        `
+    },
+    {
+        "author":"jonas",
+        "keywords":["requestAnimationFrame兼容"],
+        "code":`
+            // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+            // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+            // requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+            // MIT license
+            (function () {
+                var lastTime = 0;
+                var vendors = ['ms', 'moz', 'webkit', 'o'];
+                for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+                    window.requestAnimationFrame =
+                        window[vendors[x] + 'RequestAnimationFrame'];
+                    window.cancelAnimationFrame =
+                        window[vendors[x] + 'CancelAnimationFrame'] ||
+                        window[vendors[x] + 'CancelRequestAnimationFrame'];
+                }
+                if (!window.requestAnimationFrame)
+                    window.requestAnimationFrame = function (callback, element) {
+                        var currTime = new Date().getTime();
+                        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                        var id = window.setTimeout(function () {
+                            callback(currTime + timeToCall);
+                        }, timeToCall);
+                        lastTime = currTime + timeToCall;
+                        return id;
+                    };
+                if (!window.cancelAnimationFrame)
+                    window.cancelAnimationFrame = function (id) {
+                        clearTimeout(id);
+                    };
+            })();
+        `
+    },
+    {
+        "author":"jonas",
+        "keywords":["鼠标移动的方向", "鼠标移动方向"],
+        "code":`
+            /*
+            * 获取元素移动的方向
+            * @param  $element  元素的jQuery对象
+            * @param  event     事件对象
+            * @return direction 返回一个数字：0:上，1:右，2:下，3:左
+            **/
+            function getDirection($element, event) {
+                var w = $element.width(),
+                    h = $element.height(),
+                    x =
+                        (event.pageX - $element.offset().left - w / 2) *
+                        (w > h ? h / w : 1),
+                    y = (event.pageY - $element.offset().top - h / 2) * (h > w ? w / h : 1),
+                    direction =
+                        Math.round((Math.atan2(y, x) * (180 / Math.PI) + 180) / 90 + 3) % 4;
+            
+                return direction;
+            }
+            
+            $('#content')
+                .on('mouseenter', function (event) {
+                    console.log('enter: ' + getDirection($(this), event));
+                })
+                .on('mouseleave', function (event) {
+                    console.log('leave: ' + getDirection($(this), event));
+                });
         `
     }
 ]
