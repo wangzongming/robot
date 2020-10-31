12c896cf52508ead0a73ff87e846a616a002bfc3
xm <1746809408@qq.com>
2020-10-31 11:57:28 +0800

    bug修复

--- pages/api/js/basic.js
+++ pages/api/js/basic.js
@@ -3,7 +3,7 @@ import funcs from "./func"
 export default (req, res) => {
     res.statusCode = 200;
     const inpKeyword = req.query.keyword; //输入的关键词
-
+    console.log('输入关键词：', inpKeyword)
     forLabel:
     for (let i = 0; i < funcs.length; i++) {
         const {
@@ -14,12 +14,13 @@ export default (req, res) => {
         //模糊匹配
         const isOk = keywords.filter((item) => new RegExp(inpKeyword, "ig").test(item));
         if (isOk) {
-            res.json({ code, author });
+            console.log('匹配到数据：', funcs[i])
+            res.json({ author, code });
             break forLabel;
         } else {
             res.json({
+                author: "jonas",
                 code: `暂未获取到， 如果您有好的代码段欢迎提交github：https://github.com/wangzongming/robot`,
-                author: "jonas"
             });
         }
     }
